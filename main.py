ef18feb04c738c335e13d361b584188c833032fa
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2018-08-21 11:09:08 +0100

    Main updated to describe Stern-Gerlach with classical free particle.

--- main.py
+++ main.py
@@ -19,10 +19,16 @@ CQstate = lib.CQState(np.matrix([[1/sqrt(2)],[1/sqrt(2)]]),0.,0.)
 		- H = h0 L0^dagger L0 + h1 L1^dagger L1
 		- h0 = +q
 		- h1 = -q
+	- Classical Hamiltonian is
+		- Hc = p^2/2
+		- dHcdq = 0
+		- dHcdp = p
 """
 L0 = np.matrix([[0.,0.],[1.,0.]])
 L1 = np.matrix([[0.,1.],[0.,0.]])
 QHamlitonian = lambda q,p : q * np.matrix([[1.,0.],[0.,-1.]])
+clas_pos_derivs = lambda q,p : 0
+clas_mom_derivs = lambda q,p : p
 
 """ The derivative of h0 and h1 is
 	- dh0/dq = 1 , dh0/dp = 0
@@ -38,20 +44,20 @@ mom_derivs = [0., 0.]
 """
 tau = 1e-1
 delta_time = 1e-4
-final_time = 10.
+final_time = 1.
 
 # Random seed
 seed = 365
 rn.seed(seed)
 
 # Number of iterations
-iterations = int(1e3)
+iterations = int(1e4)
 
 for iteration in range(iterations):
 # Filename for output
 	filename = './output/output{0}.dat'.format( str(iteration) )
 
 	# Create unravelling and evolve
-	stern_gerlach = lib.Unravelling(copy(CQstate), [L0,L1], pos_derivs, mom_derivs, QHamlitonian, tau, delta_time, final_time, seed, filename)
+	stern_gerlach = lib.Unravelling(copy(CQstate), [L0,L1], pos_derivs, mom_derivs, QHamlitonian, clas_pos_derivs, clas_mom_derivs, tau, delta_time, final_time, seed, filename)
 	stern_gerlach.evolution()
 	del stern_gerlach
