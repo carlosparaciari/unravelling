402008b5d75a271fc1ff7477f67268dd19c49d50
carlosparaciari <carlo.sparaciari.14@ucl.ac.uk>
2019-05-07 16:23:31 +0100

    Example of main file created

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ from datetime import datetime
 from math import sqrt
 from copy import copy
 
+""" Stern Gerlach with diagonal Lindblad operators.
+
+	This main file, together with the ipython notebook in this repository,
+	is an example created for helping the user familiarise with the code.
+	In this main, the trajectories of a CQ system representing a qubit
+	particle interacting with its own position degrees of freedom are created.
+	In the python notebook, these trajectories are processed and visualised.
+"""
+
 """ The initial CQ state is:
-	- state : \ket{1}, eigenstate of Pauli Z with eigenvalue 1
-	- position : 0.1, start a bit displaced from the centre
+	- quantum state : \ket{+}, eigenstate of Pauli X with eigenvalue 1
+	- position : 0, start with zero position 
 	- momentum : 0, start with zero momentum 
 """
-CQstate = lib.CQState(np.matrix([[1.],[0.]]),1e-1,0.)
+CQstate = lib.CQState(np.matrix([[1./sqrt(2.) + 0.j],[1./sqrt(2.) + 0.j]]),0.,0.)
 
 """ The time scale:
 	- tau : rate of jump
@@ -18,67 +27,90 @@ CQstate = lib.CQState(np.matrix([[1.],[0.]]),1e-1,0.)
 	- final time : final time we are interested in
 """
 tau = 1e-2
-delta_time = 1e-4
-final_time = 1e-1
+delta_time = 2.5e-5
+final_time = 5e-2
 
 # Random seed
-seed = 365
+seed = 1424
 rn.seed(seed)
 
 # Number of iterations
-iterations = int(10)
+iterations = 200
 
 # Classical-quantum interaction strength
-B = 5e3
+B = 1.
+
+# Fully quantum Hamiltonian coefficient
+gamma = 1e-4
 
 """ The dynamical configuration is
-	- Lindblad :
-		- L0 send \ket{0} in \ket{0}, and |w0| = 1
-		- L1 send \ket{1} in \ket{1}, and |w1| = 1
-	- Interaction Hamiltonian
+
+	- Classical-quantum interaction Hamiltonian:
+
 		- H = h0 L0^dagger L0 + h1 L1^dagger L1
-		- h0 = +q^2 B
-		- h1 = -q^2 B
-	- Classical Hamiltonian is
+
+	- Lindblad operators:
+
+		- L0 = \sqrt(|w0|) \ket{0}\bra{0}
+		- L1 = \sqrt(|w1|) \ket{1}\bra{1}
+
+	- Coupling factors:
+
+		- h0 = sign(w0) q B
+		- h1 = sign(w1) q B
+
+	- Coupling constants:
+
+		- w0 = +1
+		- w1 = -1
+
+	- Quantum Hamiltonian:
+
+		- Hq = gamma X
+
+	- Classical Hamiltonian:
+
 		- Hc = p^2/2
-		- dHcdq = 0
-		- dHcdp = p
 """
-def interaction_Hamiltoian(B_field):
-	return lambda q,p : q**2 * B_field * np.matrix([[1.,0.],[0.,-1.]])
+def quantum_and_interaction_Hamiltonian(B_field, gamma):
+	return lambda q,p : q * B_field * np.matrix([[1.,0.],[0.,-1.]]) + gamma * np.matrix([[0.,1.],[1.,0.]])
+
+QHamlitonian = quantum_and_interaction_Hamiltonian(B,gamma)
 
 L0 = np.matrix([[1.,0.],[0.,0.]])
 L1 = np.matrix([[0.,0.],[0.,1.]])
-QHamlitonian = interaction_Hamiltoian(B)
-clas_pos_derivs = lambda q,p : 0
+
+qu_pos_derivs = [lambda q,p : B,
+				 lambda q,p : - B
+				]
+qu_mom_derivs = [lambda q,p : 0.,
+				 lambda q,p : 0.
+				]
+
+clas_pos_derivs = lambda q,p : 0.
 clas_mom_derivs = lambda q,p : p
-
-""" The derivative of h0 and h1 is
-	- dh0/dq = 2qB , dh0/dp = 0
-	- dh1/dq = -2qB , dh1/dp = 0
-"""
-def derivatives_quantum_h_position(B_field):
-	dh0dq = lambda q,p : 2 * q * B_field
-	dh1dq = lambda q,p : -2 * q * B_field
-	return [dh0dq, dh1dq]
-
-def derivatives_quantum_h_momentum():
-	dh0dp = lambda q,p : 0
-	dh1dp = lambda q,p : 0
-	return [dh0dp, dh1dp]
-
-qu_pos_derivs = derivatives_quantum_h_position(B)
-qu_mom_derivs = derivatives_quantum_h_momentum()
 
 # Measure time before simulation
 tstart = datetime.now()
 
 for iteration in range(iterations):
-# Filename for output
-	filename = './output/output{0}.dat'.format( str(iteration) )
+	# Filename for output
+	filename = './output/trajectory_{0}.dat'.format( str(iteration) )
 
 	# Create unravelling and evolve
-	stern_gerlach = lib.Unravelling(copy(CQstate), [L0,L1], qu_pos_derivs, qu_mom_derivs, QHamlitonian, clas_pos_derivs, clas_mom_derivs, tau, delta_time, final_time, seed, filename)
+	stern_gerlach = lib.Unravelling( copy(CQstate),
+									 [L0,L1],
+									 qu_pos_derivs,
+									 qu_mom_derivs,
+									 QHamlitonian,
+									 clas_pos_derivs,
+									 clas_mom_derivs,
+									 tau,
+									 delta_time,
+									 final_time,
+									 seed,
+									 filename
+								   )
 	stern_gerlach.evolution()
 	del stern_gerlach
 
